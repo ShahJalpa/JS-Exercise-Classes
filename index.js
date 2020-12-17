b2fd870346fe645938c859166e9ddc7a4532b668
jalpa Shah <jalpa-shah@lambdastudents.com>
2020-12-17 14:57:43 -0500

    just prep

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
         + If a plane takes off, its `isFlying` property gets set to true.
         + If a plane lands, its `isFlying` property gets set to false.
 */
+//just prep
 
 // EXAMPLE SOLUTION CODE:
 class Airplane {
